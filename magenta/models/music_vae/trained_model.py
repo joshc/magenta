0558da72fd006fd003acf352f1cba5880038d36f
Victor Chen <vlchen888@gmail.com>
2019-06-02 20:34:16 -0700

    Fix syntax bug

--- magenta/models/music_vae/trained_model.py
+++ magenta/models/music_vae/trained_model.py
@@ -141,7 +141,7 @@ class TrainedModel(object):
       else:
         saver.restore(self._sess, checkpoint_path)
 
-  def sample(self, n=None, length=None, temperature=1.0, same_z=False,, z=None
+  def sample(self, n=None, length=None, temperature=1.0, same_z=False, z=None,
              c_input=None, context=None):
 
     """Generates random samples from the model.
